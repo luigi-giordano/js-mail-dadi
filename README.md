f790928f23171f77b813d290db20892df30d5fb7
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-18 22:43:32 +0100

    start while

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-Mail
+Esercizio 1 "Mail"
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. Non è necessario provvedere alla validazione delle email
 
-Gioco dei dadi
+Esercizio 2 "Gioco dei dadi"
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
---
+++ script.js
@@ -0,0 +1,27 @@
+const emailAutorizzate = [
+  "simone@gmail.com",
+  "massimo@hotmail.com",
+  "stefano@icloud.com"
+];
+
+// Chiedi all'utente la sua email
+const emailInserita = prompt("Inserisci la tua email per accedere:");
+
+let message = ''
+let emailVerificate = false
+let i = 0;
+
+while (emailVerificate === false){
+  if (emailInserita === emailAutorizzate[i]){
+    emailVerificate === true
+    message = 'L\'email risulta nell\'elenco'
+  } else {
+    i++;
+  }
+  if (emailInserita !== emailAutorizzate[i]){
+    emailVerificate === false
+    message = 'L\'email non risulta nell\'elenco'
+  }
+  
+  
+}
